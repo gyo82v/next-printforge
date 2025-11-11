3848dff8bc9d1a5d25868828954a56ae628e10c4
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-11-11 19:52:31 +0100

    feat: add functionality to rouet 3d-modles/category

--- app/3d-models/categories/[categoryName]/page.tsx
+++ app/3d-models/categories/[categoryName]/page.tsx
@@ -1,11 +1,20 @@
 import type { CategoryPageProps } from "@/types";
-import { getCategoriesBySlug } from "@/lib/categories";
+import { getModels } from "@/lib/models";
+import Modelcard from "@/components/ModelCard";
 
 export default async function categoryPage({params}:CategoryPageProps){
     const {categoryName} = await params
-    const category = getCategoriesBySlug(categoryName)
+    const models = await getModels({category: categoryName})
+    const wrapper = ` w-11/12 mx-auto px-4 py-8 flex flex-col gap-6 
+                     sm:flex-wrap sm:flex-row justify-center`
+
+    const modelArr = models.map(m => (
+        <Modelcard data={m} key={m.id}/>
+    ))
     return(
-        <p>{category.displayName}</p>
+        <main className={wrapper}>
+            {modelArr}
+        </main>
     )
 
 }
--- lib/models.ts
+++ lib/models.ts
@@ -1,8 +1,20 @@
 import modelsData from "@/data/models.json"
 import type { Model } from "@/types"
 
+type Props = {
+  category?: string | undefined
+}
+
 export async function getAllModels():Promise<Model[]>{
   return modelsData
+}
+
+export async function getModels({category}:Props):Promise<Model[]>{
+  let filteredModels = [...modelsData]
+  if(category){
+    filteredModels = modelsData.filter(m => m.category === category)
+  }
+  return filteredModels
 }
 
 export async function getModelById(id: string | number):Promise<Model>{
